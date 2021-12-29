85c3e9ae1190790638c0d467bfe9f441c967a4a0
origin <alicx991117@naver.com>
2021-12-29 21:56:23 +0900

    gradle 설정 다시 만지기, html 파일 폴더 옮기기

--- backend/demo/demo/src/main/java/jikdal/User/UserController.java
+++ backend/demo/demo/src/main/java/jikdal/User/UserController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@RequiredArgsConstructor
+
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
--- backend/demo/demo/src/main/java/jikdal/User/WebSecurityConfig.java
+++ backend/demo/demo/src/main/java/jikdal/User/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package jikdal.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +17,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
 
     @Override
-    protected void configure(WebSecurity web){
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/css/**","/js/**","/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
                     .antMatchers("/login", "/signup","/user").permitAll()
